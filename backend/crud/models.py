1f819c2a7060d0f502eed15ec401f21abbcf6cbd
Mr-mnin <nadimul.nahin008@gmail.com>
2026-08-29 13:35:54 +0600

    feat: update models and serializers for Subjects and Tutor, add media URL handling

--- backend/crud/models.py
+++ backend/crud/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class 
 
-class role(models.Model):
-    name = models.CharField(unique=True, max_length=100)
+class Subjects(models.Model):
+    name = models.CharField( max_length=100)
+    slug = models.SlugField(unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Tutor(models.Model):
+    name = models.CharField( max_length=100)
+    slug = models.SlugField(unique=True)
+    portrait = models.ImageField(upload_to='tutors/portraits/', blank=True, null=True)
+    short_bio= models.TextField( max_length=50000 )
+    short_bio= models.TextField()
+    years_experience = models.IntegerField(max_length=2)
+    is_founder=models.BooleanField()
+    is_featured=models.BooleanField()
+    display_order= models.IntegerField(max_length=2)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
--- backend/crud/urls.py
+++ backend/crud/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from .views import *
 from rest_framework.routers import  DefaultRouter
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 #router = DefaultRouter()
 #router.register('', --ViewSet, basename='')
 
